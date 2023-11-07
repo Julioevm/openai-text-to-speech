ab1c1b3eca14ae7f9ec0b0a147c7276a3312f396
Julio Valls <julioevm@gmail.com>
2023-11-07 12:09:39 +0100

    Use positional argument as input if no text file is passed.

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main(input_text, voice="onyx", model="tts-1-hd", output_file="speech.mp3"):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate speech from text using OpenAI")
+    parser.add_argument("input", nargs="?", help="Text input for speech generation (use as positional argument if --text-file is not provided)")
     parser.add_argument("--text-file", help="Path to a text file with input text")
     parser.add_argument("--voice", default="onyx", help="Voice for speech generation (default: onyx)")
     parser.add_argument("--model", default="tts-1-hd", help="Model for speech generation (default: tts-1-hd)")
@@ -37,8 +38,10 @@ if __name__ == "__main__":
     if args.text_file:
         with open(args.text_file, 'r') as file:
             input_text = file.read()
+    elif args.input:
+        input_text = args.input
     else:
-        print("Please provide a text file using the --text-file argument.")
+        print("Please provide a text file using the --text-file argument or specify the input text as a positional argument.")
         exit(1)
 
     if len(input_text) > MAX_INPUT_LENGTH:
